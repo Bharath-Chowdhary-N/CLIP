f60898cc5978d7f66dbddfd87716ea6256231561
Bharath-Chowdhary-N <n.bharath.chowdhary@gmail.com>
2024-11-10 14:20:30 -0600

    added ResNet Model Summary

---
+++ CLIP.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+from torch.nn import Functional as F
+
+class image_encoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass
+    def forward():
+        pass
