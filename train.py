20400d027a3efff52d350aa3870086d76f0b77b5
Bharath-Chowdhary-N <n.bharath.chowdhary@gmail.com>
2024-11-11 15:44:31 -0600

    added structure to the code

--- train.py
+++ train.py
@@ -3,6 +3,20 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 from CLIP import CLIP
 
+def train_clip(model):
+    #add code to train the model
+    pass
+
 def main():
 
     model = CLIP(embed_dim=128, temperature=0.1)
+
+    #add code to make image text pairs
+
+    #add code to train model
+    trained_model = train_clip(model=model)
+
+    #save the trained model
+
+if __name__ == "__main__":
+    main()
