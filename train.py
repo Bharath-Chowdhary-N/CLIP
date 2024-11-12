347d86a3cc805c2ad0de1680bcdb600ef454bf0c
Bharath-Chowdhary-N <n.bharath.chowdhary@gmail.com>
2024-11-12 00:21:25 -0600

    added tokenizer

--- train.py
+++ train.py
@@ -4,16 +4,31 @@ from torch.utils.data import DataLoader
 from torchvision import transforms
 from CLIP import CLIP
 from PIL import Image
+from transformers import CLIPTokenizer as HFCLIPTokenizer
 
-def train_clip(model):
-    #add code to train the model
-    pass
+def train_clip(model, device, learning_rate=1e-4, dataset):
+
+    model = model.to(device)
+    model.train()
+
+    # Optimizer
+    optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+    criterion = nn.CrossEntropyLoss()
+
+    # Data Loader
+    train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=4)
+
+    
+
+
+
 class Image_Text_Dataset():
-    def __init__(self, image_text_pairs, image_size=224):
+    def __init__(self, image_text_pairs, image_size=224, context_length=64):
         self.image_text_pairs = image_text_pairs
+        self.context_length = context_length
         self.image_size = image_size
         self.transform = transforms.Compose([transforms.Resize((self.image_size, self.image_size)), transforms.ToTensor()])
-    
+        self.tokenizer = HFCLIPTokenizer.from_pretrained("openai/clip-vit-base-patch32")
     def __len__(self):
         return len(self.image_text_pairs)
     
@@ -24,7 +39,8 @@ class Image_Text_Dataset():
         if self.transform:
             image = self.transform(image)
         
-        return image, text
+        tokenized = self.tokenizer(text, padding="max_length", max_length = self.context_length, truncation=True, return_sensors="pt")
+        return image, tokenized["input_ids"][0]
 
 def main():
 
@@ -36,7 +52,8 @@ def main():
     dataset = Image_Text_Dataset(image_text_pairs)
 
     #add code to train model
-    trained_model = train_clip(model=model)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    trained_model = train_clip(model=model, device=device, learning_rate=1e-4, dataset=dataset)
 
     #save the trained model
 
