5e47bb5611acede2eba9b46537dc1b383e932898
agentM-GEG <km4n6@mail.umkc.edu>
2024-12-06 11:27:49 -0600

    added a continuous progress bar

--- src/Trainer.py
+++ src/Trainer.py
@@ -7,6 +7,7 @@ from torch.nn import functional as F
 from transformers import CLIPTokenizer as HFCLIPTokenizer
 import numpy as np
 import os
+import tqdm
 
 def train_clip(model, device, train_loader, learning_rate=1e-4, batch_size=16, warmup_steps=2000, num_epochs=50, enable_amp = True, dest_folder='./checkpoints'):
 
@@ -36,16 +37,22 @@ def train_clip(model, device, train_loader, learning_rate=1e-4, batch_size=16, w
     for epoch in range(num_epochs):
         total_loss = 0
         num_batches = 0
-        for batch_idx, (images, tokens) in enumerate(train_loader):
+        
+        print(f"Epoch {epoch}")
+        print("-------------------------------------------------------")
+
+        # batch loss data
+        pbar = tqdm.tqdm(train_loader, desc='Training: ', dynamic_ncols=True)
+        
+        for batch_idx, (images, tokens) in enumerate(pbar):
             images = images.to(device)
             tokens = tokens.to(device)
             with torch.cuda.amp.autocast():
-                 logits_per_image, logits_per_text = model(images,tokens)
-            
-                 labels = torch.arange(len(images)).to(device)
-                 loss_i = F.cross_entropy(logits_per_image, labels)
-                 loss_t = F.cross_entropy(logits_per_text, labels)
-                 loss = (loss_i + loss_t) / 2
+                logits_per_image, logits_per_text = model(images,tokens)
+                labels = torch.arange(len(images)).to(device)
+                loss_i = F.cross_entropy(logits_per_image, labels)
+                loss_t = F.cross_entropy(logits_per_text, labels)
+                loss = (loss_i + loss_t) / 2
             
             optimizer.zero_grad()
 
@@ -62,14 +69,16 @@ def train_clip(model, device, train_loader, learning_rate=1e-4, batch_size=16, w
             total_loss += loss.item()
             num_batches+=1
 
-            if batch_idx%100==0:
-                avg_loss = total_loss / num_batches
-                print(f"Epoch: {epoch+1}/{num_epochs}, Batch: {batch_idx}/{len(train_loader)}, "
-                      f"Loss: {avg_loss:.4f}, LR: {optimizer.param_groups[0]['lr']:.6f}, "
-                      f"Scale: {model.logit_scale.exp().item():.1f}"
-                      )
+            
+            avg_loss = total_loss / num_batches
+            loss_str = "".join(f"Loss: {avg_loss:.4f}, "
+                  f"Scale: {model.logit_scale.exp().item():.1f}")
+            pbar.set_postfix_str(loss_str)
+                
             global_step +=1
-            if epoch%5 ==0:
+        
+        print(f"Epoch {epoch+1} completed. Average Loss: {total_loss / num_batches:.4f}")
+        if epoch%5 ==0:
                 torch.save({
                 'epoch': epoch,
                 'model_state_dict': model.state_dict(),
@@ -77,7 +86,5 @@ def train_clip(model, device, train_loader, learning_rate=1e-4, batch_size=16, w
                 'loss': loss.item(),
                 #'scaler': scaler.state_dict() if enable_amp else None,
             }, f'{dest_folder}/clip_checkpoint_epoch_{epoch}.pt')
-        
-            print(f"Epoch {epoch+1} completed. Average Loss: {total_loss / num_batches:.4f}")
-    
+                print(f'Saved Model {dest_folder}/clip_checkpoint_epoch_{epoch}.pt')
     return model
